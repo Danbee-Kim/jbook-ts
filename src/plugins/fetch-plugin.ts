35461d3cc9b6a94ffb3177e554c01f75d477f36f
Danbee Kim <kdb2360@gmail.com>
2022-12-06 21:05:36 +0900

    css files 변환 추가

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -6,21 +6,18 @@ const fileCache = localForage.createInstance({
   name: "filecache",
 });
 
-export const fetchPlugin = (inputcode: string) => {
+export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      //handle main file of a module
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
+
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputcode,
-          };
-        }
-
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path,
         );
@@ -28,7 +25,31 @@ export const fetchPlugin = (inputcode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
